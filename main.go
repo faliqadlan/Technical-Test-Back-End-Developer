285b5c0230c98327607f963461d7658a4c4502e8
faliqadlan <faliq_adlan@student.ub.ac.id>
2022-04-02 12:43:28 +0700

    temp partition

--- main.go
+++ main.go
@@ -1,14 +1,7 @@
 package main
 
-import (
-	"qlue/algorithmic"
-)
+import "qlue/algorithmic"
 
 func main() {
-	// fmt.Println(algorithmic.AllSum(6))
-
-	// fmt.Println(algorithmic.PartitionSetProcess(arr, 1, 4))
-
 	algorithmic.PartitionSet(4)
-
 }
